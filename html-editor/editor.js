b81d033d1ed1e784543491edb2d3614b32d93c1e
Kalle Hallivuori <korpiq@iki.fi>
2022-01-10 18:40:18 +0200

    Let user remove elements. Place inner elements correctly.

--- html-editor/editor.js
+++ html-editor/editor.js
@@ -1,9 +1,13 @@
 function PageEditor() {
 
     var pageEditDialog = createPageEditDialog()
+    var selectedElement = document.body
+    var gridSize = {
+        x: 16,
+        y: 16,
+    }
     var isMouseOnEditDialog = false
     var contextMenuAt = {
-        target: document.body,
         x: 0,
         y: 0,
     }
@@ -46,9 +50,12 @@ function PageEditor() {
         window.onkeydown = hidePageEditDialogByEvent
         window.onmousedown = hidePageEditDialogByEvent
 
-        contextMenuAt.target = event.target
         contextMenuAt.x = event.clientX
         contextMenuAt.y = event.clientY
+        selectedElement = document.body.contains(event.target) ? event.target : document.body
+        const showAreaOptions = selectedElement !== document.body
+        const areaOptionsDialog = document.getElementById('page-editor-dialog-options-for-area')
+        areaOptionsDialog.style.display = showAreaOptions ? 'inherit' : 'none'
 
         return false
     }
@@ -77,23 +84,26 @@ function PageEditor() {
     }
 
     function createPageEditDialog() {
+        function afterHidingDialog(f, args) {
+            return () => {
+                hidePageEditDialog()
+                f(...(args || []))
+            }
+        }
         return createElement(
             'div',
             {
-                id: 'editor-dialog',
+                id: 'page-editor-dialog',
                 onmouseover: () => isMouseOnEditDialog = true,
                 onmouseout: () => isMouseOnEditDialog = false,
             },
             [
-                createElement('div', null, ['Page editor']),
-                createButton('Add div', (event) => {
-                    hidePageEditDialog()
-                    addDiv(contextMenuAt.target, contextMenuAt.x, contextMenuAt.y)
-                }),
-                createButton('Save', () => {
-                    hidePageEditDialog()
-                    savePage()
-                }),
+                createElement('div', { id: 'page-editor-dialog-options-for-area', style: { display: 'none' } }, [
+                    createButton('Delete area', afterHidingDialog(() => selectedElement.remove())),
+                    createElement('hr')
+                ]),
+                createButton('Add area', afterHidingDialog(() => addDiv(selectedElement, contextMenuAt.x, contextMenuAt.y))),
+                createButton('Save page', afterHidingDialog(savePage)),
             ],
         )
     }
@@ -101,9 +111,7 @@ function PageEditor() {
     function createElement(tagName, attributes, children) {
         const element = document.createElement(tagName)
         if (attributes) {
-            for (key in attributes) {
-                element[key] = attributes[key]
-            }
+            deepMergeObjects(attributes, element)
         }
         if (children) {
             element.append(...children)
@@ -111,18 +119,44 @@ function PageEditor() {
         return element
     }
 
+    function deepMergeObjects(source, target) {
+        for (key in source) {
+            const value = source[key]
+            if (typeof (value) === 'object') {
+                deepMergeObjects(value, target[key])
+            } else {
+                target[key] = value
+            }
+        }
+    }
+
     function createButton(innerText, onclick) {
-        return createElement('button', { innerText, onclick })
+        return createElement('div', {}, [createElement('button', { innerText, onclick })])
     }
 
     function addDiv(parent, x, y) {
-        const div = createElement('div', {
-            className: 'area-marker',
-        })
+        var offsetParent = parent
+        while (offsetParent) {
+            x -= offsetParent.offsetLeft
+            y -= offsetParent.offsetTop
+            offsetParent = offsetParent.offsetParent
+        }
+        x = Math.floor(x / gridSize.x) * gridSize.x
+        y = Math.floor(y / gridSize.y) * gridSize.y
+
+        if (x < 0 || x > (parent.offsetWidth || Infinity) || y < 0 || y > (parent.offsetHeight || Infinity)) {
+            console.log('Outside parent area!')
+            return
+        }
+
+        const div = createElement('div', { className: 'area' })
         div.style.left = x + 'px'
         div.style.top = y + 'px'
+        div.style.minWidth = gridSize.x + 'px'
+        div.style.minHeight = gridSize.y + 'px'
 
         parent.appendChild(div)
+        selectedElement = div
     }
 
     return { editPage, savePage }
